8848362759592847ccb647676bf42bb6b9dfa7ca
lyknight2 <104610404+lyknight2@users.noreply.github.com>
2023-09-06 16:10:45 +1000

    yeet?

--- aspnet-core-angular/aspnet-core/src/EventCloud.Core/Configuration/AppSettingNames.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EventCloud.Configuration
-{
-    public static class AppSettingNames
-    {
-        public const string UiTheme = "App.UiTheme";
-        public const string MaxAllowedEventRegistrationCountInLast30DaysPerUser = "EventCloud.MaxAllowedEventRegistrationCountInLast30DaysPerUser";
-    }
-}
